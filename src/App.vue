ccd7567f2d9af60155a1f6de56e33cdcdf1ce7df
Stanislav <stanislavschelokov@gmail.com>
2021-11-18 21:37:43 +0200

    iniatial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="containe">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [],
+    linkActiveClass: 'active',
+    linkExactActiveClass: 'active'
+})
+
+
